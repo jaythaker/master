9fea4f0c4a8e1e7b0e844adf9c2346b481b7520a
jaythaker <jay_thaker@hotmail.com>
2018-02-28 12:48:25 -0800

    Update blockProcessing.js
    Removed all the code which was related to UI.

--- blockProcessing.js
+++ blockProcessing.js
@@ -1,7 +1,7 @@
 blockProcessing = {
     callbacks: [],
     currentIndex: 0,
-	defaultTimeout: 30000
+    defaultTimeout: 30000
 };
 
 blockProcessing.registerCallbacks = function (processingType, callback) {
@@ -17,50 +17,39 @@ blockProcessing.resetProcessingState = function () {
 
 blockProcessing.processNext = function (processingType, dfd) {
 	if (blockProcessing.currentIndex > blockProcessing.callbacks[processingType].length - 1) {
-        dfd.resolve();
-        return;
-    }
+        	dfd.resolve();
+        	return;
+        }
 	blockProcessing.callbacks[processingType][blockProcessing.currentIndex++](dfd);
 };
 
 blockProcessing.startProcessing = function (processingType) {
 	var dfd = jQuery.Deferred();
 
-	$("#clientarea").block({
-		message: $('#blockMessage'),
-		css: { border: '3px solid #a00' }
-	});
-
+	// TODO: Notify the user that we are starting the process.
 	blockProcessing.resetProcessingState();
 
 	blockProcessing.processNext(processingType, dfd);
 
 	$.when(dfd).then(
         function (status) {
-        	//blockProcessing.UpdateMessageBlock('', false);
-        	$("#clientarea").unblock();
-        	console.log("DataSubmitAsync: UnBlocking the UI on Successfully submiting and refreshing the data.");
+		// TODO: Notify user that we timed out
         },
         function (status) {
-        	//blockProcessing.UpdateMessageBlock('', false);
-        	$("#clientarea").unblock();
-        	console.log("DataSubmitAsync: UnBlocking the UI on un-successfully submiting or refreshing the data.");
-        },
+		// TODO: Notify user on error
+	},
         function (status) {
-        	console.log("DataSubmitAsync: Refreshing the data.");
         	blockProcessing.processNext(processingType, dfd);
         }
     );
 
-	setTimeout(function () {
-		if (dfd.state() === "pending") {
-			console.log("DataSubmitAsync: Rejecting due to unable to complete the operations in given timeframe.");
-			dfd.reject();
-		}
-		else {
-			console.log("DataSubmitAsync: New or updated data submitted and refreshed successfully.");
-			dfd.resolve();
-		}
+    setTimeout(function () {
+	if (dfd.state() === "pending") {
+		dfd.reject();
+	}
+	else {
+		dfd.resolve();
+	}
 	}, blockProcessing.defaultTimeout);
 
 };
